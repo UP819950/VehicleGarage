ea1f656a851de6c73296ef95132668ff52484cbf
UP819950 <up819950@myport.ac.uk>
2020-11-10 20:03:13 +0000

    added more to testing and car

--- Main/src/Car.java
+++ Main/src/Car.java
@@ -2,15 +2,14 @@ public class Car {
 
     String carReg;
     int carMileage;
+    boolean carInsured;
+    int carFuelRemaining;
 
-
-
-    public Car(String reg, int mileage){
-
-        carReg = reg;
-        carMileage = mileage;
-
-
+    public Car(String carReg, int carMileage, boolean carInsured, int carFuelRemaining) {
+        this.carReg = carReg;
+        this.carMileage = carMileage;
+        this.carInsured = carInsured;
+        this.carFuelRemaining = carFuelRemaining;
     }
 
     public String getCarReg() {
@@ -29,14 +28,43 @@ public class Car {
         this.carMileage = carMileage;
     }
 
+    public boolean isCarInsured() {
+        return carInsured;
+    }
+
+    public void setCarInsured(boolean carInsured) {
+        this.carInsured = carInsured;
+    }
+
+    public int getCarFuelRemaining() {
+        return carFuelRemaining;
+    }
+
+    public void setCarFuelRemaining(int carFuelRemaining) {
+        this.carFuelRemaining = carFuelRemaining;
+    }
+
 
 
 
     @Override
     public String toString() {
+
+        String yesNo;
+
+        if(carInsured == true){
+            yesNo = "Yes";
+        } else {yesNo = "WARNING UNINSURED";};
+
+
+
+
         return
                 "carReg: " + carReg +
-                ", carMileage: " + carMileage ;
+                ", carMileage: " + carMileage +
+                ", carInsured: " + yesNo +
+                ", carFuelRemaining: " + carFuelRemaining
+                ;
     }
 
 
--- Main/src/MainTesting.java
+++ Main/src/MainTesting.java
@@ -8,18 +8,18 @@ public class MainTesting {
 
     public static void main (String[] args ){
 
-        ArrayList cars = new ArrayList();
-        Car c1 = new Car("ER12 DOT", 65993);
-        Car c2 = new Car("PO19 ERR", 12922);
+        ArrayList vehicles = new ArrayList();
+        Car c1 = new Car("ER12 DOT", 65993, true , 25);
+        Car c2 = new Car("PO19 ERR", 12922, false , 11);
 
 
 
-        cars.add(c1);
-        cars.add(c2);
+        vehicles.add(c1);
+        vehicles.add(c2);
 
-        for (int i = 0 ; i < cars.size() ; i ++  ){
+        for (int i = 0 ; i < vehicles.size() ; i ++  ){
 
-            System.out.println(  cars.get(i).toString()  );
+            System.out.println(  vehicles.get(i).toString()  );
 
         }
 
