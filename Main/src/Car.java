166bf0120724796a04a2501fceba544fa6140571
UP819950 <up819950@myport.ac.uk>
2020-11-10 19:47:22 +0000

    added data to car class

--- Main/src/Car.java
+++ Main/src/Car.java
@@ -1,2 +1,46 @@
 public class Car {
+
+    String carReg;
+    int carMileage;
+
+
+
+    public Car(String reg, int mileage){
+
+        carReg = reg;
+        carMileage = mileage;
+
+
+    }
+
+    public String getCarReg() {
+        return carReg;
+    }
+
+    public void setCarReg(String carReg) {
+        this.carReg = carReg;
+    }
+
+    public int getCarMileage() {
+        return carMileage;
+    }
+
+    public void setCarMileage(int carMileage) {
+        this.carMileage = carMileage;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "carReg='" + carReg + '\'' +
+                ", carMileage=" + carMileage +
+                '}';
+    }
+
+
+
+
 }
